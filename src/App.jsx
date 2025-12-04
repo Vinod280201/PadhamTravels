660377471147e2640d6f650aac35bc5b309417fa
Vinod M <133590606+Vinod280201@users.noreply.github.com>
2025-12-04 12:40:56 +0530

    Fix import statements in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 import { LandingPage } from './Pages/LandingPage'
-=======
-import './App.css'
-import { LandingPage } from './pages/LandingPage'
->>>>>>> fe49f646b4cd65a9a1137c6d46624521fdfe5265
 
 function App() {
   return (
